6e78767ef9197a67e6c3e7c1c8c629f7057f963d
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-09-24 19:41:27 +0300

    code along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,64 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  // function handleFirstNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     firstName: event.target.value,
+  //   });
+  // }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  // function handleLastNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     lastName: event.target.value,
+  //   });
+  // }
+  function handleChange(event){
+      // name is the KEY in of the formData object we're trying to update
+    const name = event.target.name;
+    let value = event.target.value;
+
+      // use `checked` property of checkboxes instead of `value`
+      if (event.target.type === "checkbox") {
+        value = event.target.checked;
+      }
+
+      // event.target.type ? "checkbox" ? event.target.checked :event.target.value
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+            <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
     </form>
   );
 }
-
 export default Form;
